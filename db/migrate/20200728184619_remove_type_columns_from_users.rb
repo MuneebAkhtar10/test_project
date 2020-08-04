e4d547c275f095fd70066b93270a654338be9624
Muneeb Akhtar <m.muneebakhtar1998@gmail.com>
2020-08-04 13:02:44 +0500

    heroku deploy

--- db/migrate/20200728184619_remove_type_columns_from_users.rb
+++ db/migrate/20200728184619_remove_type_columns_from_users.rb
@@ -1,6 +1,6 @@
 class RemoveTypeColumnsFromUsers < ActiveRecord::Migration[6.0]
   def change
-  	remove_column :users, :qa
+  	# remove_column :users, :qa
   	remove_column :users, :projectmanager
   	remove_column :users, :developer
   end
